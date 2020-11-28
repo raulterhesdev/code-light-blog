27f3f90d8a5516afa39394c09a552fb319780c1c
Raul Terhes <raulterhesdevelopment@gmail.com>
2020-11-28 17:16:45 +0200

    added google gtag plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,17 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-EW4RZ6L0YM', // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: path.join(__dirname, `src`, `pages`),
