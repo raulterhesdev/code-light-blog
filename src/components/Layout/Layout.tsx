16d52f74c75255a3afe321c947df753da1220fab
Raul Terhes <raulterhesdevelopment@gmail.com>
2020-11-28 17:01:00 +0200

    added side bar with short about + place for ads

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 
-type LayoutTypes = {};
+import { AllPagesEnum } from '../../types/AllPagesEnum';
 
-const Layout: React.FC<LayoutTypes> = ({ children }) => {
+type LayoutTypes = { page?: AllPagesEnum };
+
+const Layout: React.FC<LayoutTypes> = ({ children, page }) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main style={{ display: 'flex' }}>
+        <div>{children}</div>
+        <aside>
+          {page !== AllPagesEnum.about && (
+            <div>
+              <h3>About me</h3>
+              <p>Short about me description</p>
+              <Link to='/about'>Find out more!</Link>
+            </div>
+          )}
+          <div>{/* adds go here */}</div>
+        </aside>
+      </main>
       <Footer />
     </>
   );
