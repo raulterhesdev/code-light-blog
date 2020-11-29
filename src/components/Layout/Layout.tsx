505cf7adc0e09e265f51523a7658b1a73b20f100
Raul Terhes <raulterhesdevelopment@gmail.com>
2020-11-29 14:03:19 +0200

    added global style & theme

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,21 +4,41 @@ import { Link } from 'gatsby';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import CookieConsent from '../common/CookieConsent/CookieConsent';
-
+import { theme } from '../../utils/theme';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import { AllPagesEnum } from '../../types/AllPagesEnum';
 
 type LayoutTypes = { page?: AllPagesEnum };
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 16px;
+  }
+`;
+
 const Layout: React.FC<LayoutTypes> = ({ children, page }) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Helmet>
         <script
           data-ad-client='ca-pub-7374604984496320'
           async
           src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
         ></script>
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Montserrat&display=swap'
+          rel='stylesheet'
+        ></link>
       </Helmet>
+      <GlobalStyle />
       <CookieConsent />
       <Header />
       <main style={{ display: 'flex' }}>
@@ -35,7 +55,7 @@ const Layout: React.FC<LayoutTypes> = ({ children, page }) => {
         </aside>
       </main>
       <Footer />
-    </>
+    </ThemeProvider>
   );
 };
 
---
+++ src/utils/theme.ts
@@ -0,0 +1,14 @@
+export const theme = {
+  black: '#06090E',
+  darkerGrey: '#1A2639',
+  darkGrey: '#253756',
+  white: '#f8f8f8',
+  lightGrey: '#ebebeb',
+  lighterGrey: '#d6d6d6',
+  primary: '#1A8B9D',
+  primaryLight: '#1FABC1',
+  primaryDark: '#146D7B',
+  accent: '#B2D430',
+  accentLight: '#C1DC56',
+  accentDark: '#9DBA26',
+};
