e9fecfe9d40a028a038a7c81dec87102b8e29df9
Raul Terhes <raulterhesdevelopment@gmail.com>
2020-12-09 17:19:52 +0200

    updated title

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,13 +3,13 @@ module.exports = {
   postsPerPage: 4, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
   featuredPosts: 6,
   latestPosts: 1,
-  siteTitleMeta: `Code Light`, // This allows an alternative site title for meta data for pages.
+  siteTitleMeta: `Raul Terhes`, // This allows an alternative site title for meta data for pages.
   siteDescriptionMeta: `Code articles, tutorials, blog posts and more!`, // This allows an alternative site description for meta data for pages.
 
   shareImageWidth: 1000, // Change to the width of your default share image
   shareImageHeight: 523, // Change to the height of your default share image
 
-  shortTitle: `Code-Light`, // Used for App manifest e.g. Mobile Home Screen
+  shortTitle: `Raul Terhes`, // Used for App manifest e.g. Mobile Home Screen
   siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
   backgroundColor: `#f8f8f8`, // Used for Offline Manifest
   themeColor: `#253756`, // Used for Offline Manifest
