5074491ea2a3321aa6c23262502043e0e9dd01e8
Raul Terhes <raulterhesdevelopment@gmail.com>
2020-12-13 16:34:55 +0200

    moved styling to separte file

--- src/components/common/CookieConsent/CookieConsent.tsx
+++ src/components/common/CookieConsent/CookieConsent.tsx
@@ -1,47 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  z-index: 1000;
-  background-color: ${({ theme }) => theme.darkerGrey};
-  padding: 24px 16px;
-  color: ${({ theme }) => theme.white};
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: 0.8rem;
-
-  p {
-    a {
-      color: ${({ theme }) => theme.primary};
-      text-decoration-color: ${({ theme }) => theme.primary};
-    }
-  }
-`;
-
-const AcceptButton = styled.button`
-  border: none;
-  outline: none;
-  padding: 12px 18px;
-  background-color: ${({ theme }) => theme.primary};
-  color: ${({ theme }) => theme.white};
-  cursor: pointer;
-  margin-top: 24px;
-`;
-
-const DeclineButton = styled.button`
-  border: none;
-  outline: none;
-  padding: 12px 18px;
-  background-color: transparent;
-  color: ${({ theme }) => theme.lighterGrey};
-  cursor: pointer;
-`;
+import { AcceptButton, DeclineButton, Wrapper } from './CookieConsent.styles';
 
 const CookieConsent: React.FC = () => {
   const [shouldDisplayCookieBanner, setShouldDisplayCookieBanner] = useState(
